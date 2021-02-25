92cc44a336520f4b319b9181921aec355f11e329
ali-nobar <anobar@yahoo.com>
2021-02-25 11:57:44 +1100

    changed title of the project

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,9 +41,9 @@ class Index extends React.Component {
             <div className="spotlight">
               <div className="content">
                 <header className="major">
-                  <h2>NoCon</h2>
+                  <h2>No Contract Project</h2>
                 </header>
-                <p>NoCon is a DAO (Decentralised Autonomous Organisation) and gonverned by the community of stakeholders. This means NoCon is:</p>
+                <p>NoContract (NoCon) is a DAO (Decentralised Autonomous Organisation) and gonverned by the community of stakeholders. This means NoCon is:</p>
                 <ul>
                 <li>Decentralised: Not governed by one single, centralised entit</li>
                 <li>Autonomous: NoCon can automatically execute operations on its own without any intervention.</li>
